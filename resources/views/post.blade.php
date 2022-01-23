a91c7fc534fa58af41597b34968e6b5ebb14cc43
Dillon A. John <dillon.a.john@gmail.com>
2022-01-23 05:49:22 -0500

    7 - Make a Route and Link to It
    - Added new Post and Posts routes
    - Added new post and posts pages
    - Added some new css to style posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/posts">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam exercitationem eligendi culpa suscipit facere officiis numquam modi aliquid, illo, quaerat sequi! Dicta porro velit itaque delectus iure dolorem temporibus obcaecati!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur a et officiis aliquam vel ex, quaerat quas voluptatibus adipisci voluptatem voluptate eaque quidem tempora dolores saepe! Repudiandae odio nam in!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam ea quia dolor officiis harum, accusantium dolore reprehenderit, nostrum, reiciendis tempora nam vitae. Modi minus commodi eum delectus consequatur mollitia rerum?</p>
+    </article>
+
+    <a href="/posts">Go Back</a>
+</body>
+</html>
