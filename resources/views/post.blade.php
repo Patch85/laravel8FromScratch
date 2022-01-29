1c59e022f8831dd21a94f93cb51c4d94ce8ef354
Dillon A. John <dillon.a.john@gmail.com>
2022-01-29 12:38:09 -0500

    14 - Blade: The Absolute Basics
    - Refactored post and posts views to use some blade templating

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog: <?= $post->title ?></title>
+    <title>DJOHN: Blog - {{ $post->title }}</title>
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
     <article>
-       <h1><?= $post->title ?></h1>
+       <h1>{{ $post->title }}</h1>
 
-       <?= $post->body ?>
+       {!! $post->body !!}
     </article>
 
     <a href="/posts">Go Back</a>
