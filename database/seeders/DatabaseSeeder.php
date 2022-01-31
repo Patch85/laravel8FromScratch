90c7d9f2d68f9f6d3b7bf66e4e0703cf4f4ab567
Dillon A. John <dillon.a.john@gmail.com>
2022-01-31 06:09:33 -0500

    28 - Turbo Boost with Factories
    - Added new CategoryFactory and PostFactory, using faker to generate
      ad hoc categories and posts on the fly
    - Changed the DatabaseSeeder logic to utilize the new factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,70 +16,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        $users = User::factory(6)->create();
+        $categories = Category::factory(4)->create();
 
-        $user = \App\Models\User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eius architecto corrupti consectetur maiores at quo dolor adipisci cupiditate perspiciatis earum
-                alias quisquam natus soluta in ut eveniet, quasi, perferendis eum.",
-            'body' => "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem rerum repellat, pariatur
-                suscipit esse facilis corporis nisi et soluta molestias quia similique cupiditate doloribus est
-                non consequuntur? Minima, dolor et. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Et maxime nobis autem, rerum corrupti ipsum voluptatem placeat similique expedita quia vitae temporibus
-                itaque cupiditate mollitia repellat nihil odio, voluptate aspernatur.</p>"
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eius architecto corrupti consectetur maiores at quo dolor adipisci cupiditate perspiciatis earum
-                alias quisquam natus soluta in ut eveniet, quasi, perferendis eum.",
-            'body' => "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem rerum repellat, pariatur
-                suscipit esse facilis corporis nisi et soluta molestias quia similique cupiditate doloribus est
-                non consequuntur? Minima, dolor et. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Et maxime nobis autem, rerum corrupti ipsum voluptatem placeat similique expedita quia vitae temporibus
-                itaque cupiditate mollitia repellat nihil odio, voluptate aspernatur.</p>"
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => "Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eius architecto corrupti consectetur maiores at quo dolor adipisci cupiditate perspiciatis earum
-                alias quisquam natus soluta in ut eveniet, quasi, perferendis eum.",
-            'body' => "<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem rerum repellat, pariatur
-                suscipit esse facilis corporis nisi et soluta molestias quia similique cupiditate doloribus est
-                non consequuntur? Minima, dolor et. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Et maxime nobis autem, rerum corrupti ipsum voluptatem placeat similique expedita quia vitae temporibus
-                itaque cupiditate mollitia repellat nihil odio, voluptate aspernatur.</p>"
-        ]);
+        $a = 1;
+        while ($a <= 15) {
+            Post::factory()->create([
+                'user_id' => $users->random(),
+                'category_id' => $categories->random(),
+            ]);
+            $a++;
+        }
     }
 }
