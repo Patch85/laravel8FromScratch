e0a0d668544dac99f4c55e105e33bd3b63291d78
Dillon A. John <dillon.a.john@gmail.com>
2022-02-05 10:36:48 -0500

    HTML & CSS Workshop 2
    - Top article section

--- resources/views/index.php
+++ resources/views/index.php
@@ -85,8 +85,51 @@
             </div>
         </header>
 
-        <main>
+        <main class="max-w-6xl mx-auto mt-20">
+            <article class="bg-gray-100 rounded-xl">
+                <div class="py-6  px-4 flex">
+                    <div class="flex-1 mr-8">
+                        <img src="/images/illustration-1.png" alt="Blog post illustration 1" class="rounded-xl">
+                    </div>
 
+                    <div class="flex-1 flex flex-col justify-between">
+                        <header>
+                            <div class="space-x-2">
+                                <a href="#" class="px-2 py-1 border border-blue-300 text-xs text-blue-300 rounded-full uppercase font-semibold">Techniques</a>
+
+                                <a href="#" class="px-2 py-1 border border-red-300 text-xs text-red-300 rounded-full uppercase font-semibold">Updates</a>
+                            </div>
+
+                            <div class="mt-4">
+                                <h1 class="text-3xl">This is a big title and it will look great on two or even three lines. Woohoo!</h1>
+                                <span class="mt-2 block text-gray-400 text-xs">
+                                    <time datetime="2020-11-17">November 17, 2020</time>
+                                </span>
+                            </div>
+                        </header>
+
+                        <div class="text-sm mt-2">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo neque maxime excepturi reprehenderit. Et reiciendis dicta dignissimos dolor optio? Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet alias sint suscipit ex accusantium eaque, accusamus corporis expedita autem!</p>
+
+                            <p class="mt-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et reiciendis dicta dignissimos dolor optio?</p>
+                        </div>
+
+                        <footer class="flex justify-between items-center mt-2">
+                            <div class="flex items-center text-sm">
+                                <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                                <div class="ml-3">
+                                    <h5 class="font-bold">Lary Laracore</h5>
+                                    <h6>Mascot at Laracasts</h6>
+                                </div>
+                            </div>
+
+                            <div>
+                                <a href="#" class="text-sm font-semibold bg-gray-200 rounded-full py-2 px-6">Read More</a>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </article>
         </main>
 
         <footer>
