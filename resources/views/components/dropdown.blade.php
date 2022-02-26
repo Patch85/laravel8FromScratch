1e44920b5e4d6757422ef1f3a009231a63fc6115
Dillon A. John <dillon.a.john@gmail.com>
2022-02-26 15:17:41 -0500

    35 - How to Extract a Dropdown Blade Component

---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,14 @@
+@props(['trigger', 'links'])
+
+<div x-data="{ show: false }" @click.away="show = false">
+
+    {{-- Trigger --}}
+    <div @click="show = ! show">
+        {{ $trigger }}
+    </div>
+
+    {{-- Dropdown Links --}}
+    <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
+        {{ $links }}
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::redirect('/', '/posts');
+Route::redirect('/', '/posts')->name('home');
 
 Route::get('/posts', function () {
     return view('posts', [
         'posts' => Post::latest()->get(),
         'categories' => Category::all(),
     ]);
-});
+})->name('posts');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' =>  $post,
         'categories' => Category::all(),
     ]);
-});
+})->name('post');
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
@@ -38,11 +38,11 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all(),
     ]);
-});
+})->name('category');
 
 Route::get('/authors/{author:name}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
         'categories' => Category::all(),
     ]);
-});
+})->name('author');
