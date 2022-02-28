ee4f191d2733b4bf3ea7d84ffd31943e7a0abadf
Dillon A. John <dillon.a.john@gmail.com>
2022-02-28 18:34:23 -0500

    42 - Merge Category and Search Queries

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -25,6 +25,11 @@
             <!-- Search -->
             <span class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
                 <form action="#" method="get">
+
+                    @if (request('category'))
+                        <input type="hidden" name="category" value="{{ request('category') }}">
+                    @endif
+
                     <input type="text" name="search" class="bg-transparent placeholder-black text-sm font-semibold"
                         placeholder="Find something" value="{{ request('search') }}">
                 </form>
