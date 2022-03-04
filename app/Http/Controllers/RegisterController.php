7ad1ded7e6749eb7ba903fe78f28267daf72bc1b
Dillon A. John <dillon.a.john@gmail.com>
2022-03-03 21:40:04 -0500

    45 - Build a Register User Page

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return Redirector|RedirectResponse
+     * @throws BindingResolutionException
+     */
+    public function store(Request $request)
+    {
+        $input = $request
+            ->validate([
+                'name' => ['required', 'max:255'],
+                'email' => ['required', 'email', 'max:255'],
+                'password' => ['required', 'min:7', 'max:255'],
+            ], $request->only([
+                'name',
+                'email',
+                'password',
+            ]));
+
+        User::create($input);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+// Home
+
 Route::redirect('/', '/posts')->name('home');
+
+
+
+// Posts
 
 Route::get('/posts', [PostController::class, 'index'])->name('posts');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('post');
+
+
+
+// User Registration
+
+Route::get('/register', [RegisterController::class, 'create']);
+
+Route::post('/register', [RegisterController::class, 'store']);
