f5c6d77ae2d6cd1998315c631ed05e96c2fa4a8d
Dillon A. John <dillon.a.john@gmail.com>
2022-03-13 12:08:16 -0400

    52 - Write the Markup for a Post Comment

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,19 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="random avatar" width="60" class="rounded-xl">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ea praesentium
+            laboriosam, repudiandae quisquam ipsam dolorem dolores cum, officiis beatae, tempore
+            iusto. Atque quis dolorem maxime in, quam amet est!</p>
+    </div>
+</article>
